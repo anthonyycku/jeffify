d4abdf23360be96ca28ede67d9a4060e9e57b023
Anthony Ku <anthony.yc.ku@gmail.com>
2021-02-04 22:26:34 -0500

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,5 @@
 Rails.application.routes.draw do
 
-  get '/companies', to: 'companies#index'
-  get '/companies/:id', to: 'companies#show'
-  post '/companies', to: 'companies#create'
-  put '/companies/:id', to: 'companies#update'
-  delete '/companies/:id', to: 'companies#delete'
-  get '/filter/country', to:'companies#filterCountry'
-  get '/filter/countryDesc', to:'companies#filterCountryDesc'
-  get '/filter/alphabeticalASC', to:'companies#alphabeticalASC'
-  get '/filter/alphabeticalDesc', to:'companies#alphabeticalDesc'
-  #count cars
-  #count
-  get '/filter/countcarsASC', to:'companies#countcarsASC'
-  get '/filter/countcarsDESC', to:'companies#countcarsDESC'
-
-  get '/cars', to:'cars#index'
-  get '/singleCar/:id', to:'cars#showSingle'
-  get '/cars/:id', to:'cars#show'
-  post '/cars', to:'cars#create'
-  delete '/cars/:id', to:'cars#delete'
-  put '/cars/:id', to:'cars#update'
-  # Filter routes
-  get '/filter/type/:id/:type', to:'cars#filterType'
-
+  get '/songs', to: 'songs#index'
 
 end
