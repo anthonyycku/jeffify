91557d6ba85eea883c242426d7960c798699febf
Anthony Ku <anthony.yc.ku@gmail.com>
2021-02-08 17:48:43 -0500

    dont know my own words

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -22,7 +22,7 @@ class Artist < ApplicationRecord
         }
       end
 
-      def self.getAllSongs(id)
+      def self.getall(id)
         results = DB.exec(
             <<-SQL
             SELECT songs.*
