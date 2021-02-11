42853f19c6f591e037d4b57ac204e3cc1587879d
Anthony Ku <anthony.yc.ku@gmail.com>
2021-02-11 15:53:27 -0500

    im stupid

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,4 +1,10 @@
 class Account < ApplicationRecord
+    if(ENV['DATABASE_URL'])
+        uri = URI.parse(ENV['DATABASE_URL'])
+        DB = PG.connect(uri.hostname, uri.port, nil, nil, uri.path[1..-1], uri.user, uri.password)
+      else
+        DB = PG.connect(host: "localhost", port: 5432, dbname: 'jeffify_development')
+      end
 
     def self.index
         results = DB.exec(
