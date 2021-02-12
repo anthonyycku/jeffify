0288fe53424225cabe54352d1cdb96349a1c6316
Anthony Ku <anthony.yc.ku@gmail.com>
2021-02-12 14:42:36 -0500

    skip authentic

--- app/controllers/playlists_controller.rb
+++ app/controllers/playlists_controller.rb
@@ -1,5 +1,5 @@
 class PlaylistsController < ApplicationController
-
+    skip_before_action :verify_authenticity_token
     def create
         render json: Playlist.create(params["playlist"])
     end
