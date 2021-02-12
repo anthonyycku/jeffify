588ee02884eb431eb914f7583a9c231d66f030f8
Anthony Ku <anthony.yc.ku@gmail.com>
2021-02-11 21:38:30 -0500

    added ID

--- app/models/account.rb
+++ app/models/account.rb
@@ -14,6 +14,7 @@ class Account < ApplicationRecord
         )
         return results.map do |result|
             {
+                "id"=> result["id"],
                 "username" => result["username"],
                 "password" => result["password"]
             }
