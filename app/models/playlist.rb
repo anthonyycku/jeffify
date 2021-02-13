fd4ce02689941ac8b52cecb39b0d72366e7b4ccb
Anthony Ku <anthony.yc.ku@gmail.com>
2021-02-12 22:18:46 -0500

    added id

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -63,10 +63,11 @@ class Playlist < ApplicationRecord
             <<-SQL
                 INSERT INTO userplaylists (song_id, playlist_id)
                 VALUES (#{opts["song_id"]}, #{opts["playlist_id"]})
-                RETURNING song_id, playlist_id
+                RETURNING id, song_id, playlist_id
             SQL
         )
         return {
+            "id" => results.first["id"].to_i,
             "song_id" => results.first["song_id"].to_i,
             "playlist_id" => results.first["playlist_id"].to_i
         }
